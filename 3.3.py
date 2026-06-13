ac551258eb53ab816281cf82be338ab2872f7f2c
hyperGG32 <196324859+hyperGG32@users.noreply.github.com>
2026-06-13 11:46:07 +0000

    Update 3.3.py

--- 3.3.py
+++ 3.3.py
@@ -1,49 +1,35 @@
-import sys
+from collections import deque
 from collections import defaultdict
-sys.setrecursionlimit(10**6)
 
-
-v, e = map(int, input().split())
+v, e, s, t = map(int, input().split())
 adj = defaultdict(list)
+
 for _ in range(e):
     u, to = map(int, input().split())
     adj[u].append(to)
 
-color = {i: 0 for i in range(1, v + 1)}
-parent = {}
-cycle_start, cycle_end = -1, -1
+q = deque([s])
+parent = {s: None}
+found = False
 
+while q:
+    curr = q.popleft()
+    if curr == t:
+        found = True
+        break
+    for nxt in adj[curr]:
+        if nxt not in parent:
+            parent[nxt] = curr
+            q.append(nxt)
 
-def dfs(node):
-    global cycle_start, cycle_end
-    color[node] = 1
-    for nxt in adj[node]:
-        if color[nxt] == 0:
-            parent[nxt] = node
-            if dfs(nxt):
-                return True
-        elif color[nxt] == 1:
-            cycle_start = nxt
-            cycle_end = node
-            return True
-    color[node] = 2
-    return False
-
-
-for i in range(1, v + 1):
-    if color[i] == 0:
-        if dfs(i):
-            break
-
-if cycle_start == -1:
-    print("NO CYCLE")
+if not found:
+    print("UNREACHABLE")
 else:
-    cycle = []
-    curr = cycle_end
-    while curr != cycle_start:
-        cycle.append(curr)
+    path = []
+    curr = t
+    while curr is not None:
+        path.append(curr)
         curr = parent[curr]
-    cycle.append(cycle_start)
-    cycle.reverse()
-    print("CYCLE FOUND")
-    print(" ".join(map(str, cycle)))
+    path.reverse()
+    print(len(path))
+    print(" ".join(map(str, path)))
